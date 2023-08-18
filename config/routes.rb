7361e3fcaa0c8dcf459e7a181a39f84e9c1f6e96
Benoit Dassy <benoit.dassy@eezee-it.com>
2023-08-18 14:59:53 +0200

    Correction routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
-  resources :lists, only: %i[index show new create destroy] do
+  root 'lists#index'
+  resources :lists, only: %i[show new create destroy] do
     resources :bookmarks, only: %i[new create]
   end
   resources :bookmarks, only: :destroy
